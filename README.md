29b40ebb09f6ce1148f4863bc3584ac2e737a24d
emmanuelbayu <emmanuelbayu26@gmail.com>
2021-08-08 09:56:45 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Hello!
+
+This is my first project related to computer vision, which is object detection using opencv. It allows the webcam of our laptop/computer to detect objects.\
+\
+For the dataset, we will be using coco dataset which can be found [here](https://cocodataset.org/#home).\
+\
+For this project, the tools that we are going to be using is\
+![Python](https://img.shields.io/badge/-Python-05122A?style=flat&logo=python)
---
+++ main.py
@@ -0,0 +1,32 @@
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+classNames = []
+classFile = 'coco.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0/127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+while True:
+    succes, img = cap.read()
+    classIds, confs, bbox = net.detect(img, confThreshold=0.5)
+    print(classIds, bbox)
+
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            cv.rectangle(img, box, color=(0, 255, 0), thickness=2)
+            cv.putText(img, classNames[classId - 1], (box[0] + 10, box[1] + 30), fontFace=cv.FONT_HERSHEY_COMPLEX, fontScale=0.5, color=(0, 255, 0))
+    
+    cv.imshow('Output', img)
+    cv.waitKey(1)
